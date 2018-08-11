6b549d99067a85645b9819e20ed5429e74a57d53
gurk <goksu.akguc@yahoo.com>
2018-08-11 10:49:53 -0700

    ok

--- arguements.js
+++
@@ -1,5 +0,0 @@
-// console.log(parseFloat(process.argv [2]) + parseFloat(process.argv [3]));
-const firstVar = process.argv [2];
-const secondVar = process.argv [3];
-
-console.log(firstVar === secondVar);
